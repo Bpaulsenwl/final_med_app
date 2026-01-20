803a3276f5ef5f4acc3d0a903f544d5eb6af4945
Bjorrn Paulsen <bjoern@familiepaulsen.de>
2026-01-20 09:59:09 -0500

    added db creds

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:<your-password>@127.0.0.1:27017";
+const mongoURI =  "root:N7UhUCqlnSwuZANptnwGoEoO@172.21.23.68:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
