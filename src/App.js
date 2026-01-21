5cec821ee7845cd8aa2797fca31bff666d41a7f0
Bjoern Paulsen <bjoern@familiepaulsen.de>
2026-01-21 08:33:36 -0500

    fix: fixed folder name

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Navbar from './Components/Navbar/Navbar';
 import Landing_Page from './Components/Landing_Page/Landing_Page';
 import Sign_Up from './Components/Sign_Up/Sign_up';
 import Login from './Components/Login/Login';
-import FindDoctorSearch from './Components/FndDoctorSearch/FindDoctorSearch';
+import FindDoctorSearch from './Components/FindDoctorSearch/FindDoctorSearch';
 
 // Function component for the main App
 function App() {
